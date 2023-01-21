c31fbe9468dd26b6905c33efa0ddf73fa107c825
kavuthamapriya <priyarajendran018@gmail.com>
2023-01-20 20:35:05 -0330

    four

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ const about = () => {
     return(
         <div className='ui raised very padded text container segment' style={{marginTop:'80px'}}>
             <h1>About us</h1>
-            <p>Hii guys, this is kavuthamapriya. I have learning React JS and practicing more small live projects...</p>
+            <p>Hii guys, this is kavuthamapriya Rajendran. I have learning React JS and practicing more small live projects...</p>
         </div>
     )
 }
